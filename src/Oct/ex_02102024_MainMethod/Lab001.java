d7e9918b5e00dfcdff6f8ba82b2f041afb6dd502
Prashant2851993 <prashantnarnaware99@gmail.com>
2025-03-23 16:27:32 +0530

    testing

--- src/Oct/ex_02102024_MainMethod/Lab001.java
+++ src/Oct/ex_02102024_MainMethod/Lab001.java
@@ -5,7 +5,7 @@ package Oct.ex_02102024_MainMethod;
 public class Lab001 {
     public static void main(String[] args){
         System.out.println("HelloWorld!");
-         testestte
+       //  testestte
 
     }
 
---
+++ src/Oct/ex_02102024_MainMethod/kk.java
@@ -0,0 +1,7 @@
+package Oct.ex_02102024_MainMethod;
+
+public class kk {
+    public static void main(String[] args) {
+        System.out.println("Hi");
+    }
+}
