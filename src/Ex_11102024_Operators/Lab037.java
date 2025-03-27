0d31a8003177c08efe30fa021ec89747ee9b9dd3
Prashant2851993 <prashantnarnaware99@gmail.com>
2025-03-27 20:39:53 +0530

    11th October

---
+++ src/Ex_11102024_Operators/Lab037.java
@@ -0,0 +1,11 @@
+package Ex_11102024_Operators;
+
+public class Lab037 {
+    public static void main(String[] args) {
+        int a = 10;
+        // = -> assignment operator
+        // RHV to the LH
+
+    }
+
+}
